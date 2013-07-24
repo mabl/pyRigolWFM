mabl/pyRigolWFM

commit df8ee748240627eb535ab5e3e32b1c00668b3210

Add support for wfm files without the global smpRate field.
As reported by issue #1, there are wfm files which miss the
smpRate field. Since the information is redundant, we can
simply ignore the field for such files.

Detection of the protocol version is done via the file
length.

wfm.py
@@ -4,6 +4,7 @@ import collections
 import struct
 import array
 import sys
+import os
 
 # Copyright (c) 2013, Matthias Blaicher
 # All rights reserved.
@@ -159,20 +160,57 @@ def parseRigolWFM(f, strict=True):
     ("fooG",     "9s", ("expect", "==", b'\x00'*9)),
     ("points2",  "i", None),
     
-    ("time2",    "nested", time_header),
-    ("smpRate",  "f",  ("require", ">=", 0))
+    ("time2",    "nested", time_header)
   )
   
-  fileHdr = _parseFile(f, wfm_header, strict=strict)
+  # There are two known versions of the WFM file format:
+  # 1. The presumably older version does not include the smpRate 
+  #    field.
+  # 2. The smpRate field is added between the time2 header and
+  #    the channel data.
   
-  #import pprint
-  #pprint.pprint(fileHdr)
+  wfm_header_append_v2 = (
+    ("smpRate",  "f",  ("require", ">=", 0)),
+  )
+  
+
+  
+  fileHdr = _parseFile(f, wfm_header, strict=strict)
   
   # Add some simple access helpers for the repeating fields
   fileHdr["channels"] = (fileHdr["channel1"], fileHdr["channel2"])
   fileHdr["points"] = (fileHdr["points1"], fileHdr["points2"])
   fileHdr["triggers"] = (fileHdr["trigHdr1"], fileHdr["trigHdr2"])
   fileHdr["times"] = (fileHdr["time1"], fileHdr["time2"])
+  
+  totalPoints = 0
+  for channel in range(2):
+    if fileHdr["channels"][channel]['written']:
+      totalPoints = totalPoints + fileHdr["points"][channel]
+  
+  # #
+  # Detect file version based on file length
+  
+  # Extract the remaining bytes in the file
+  filePosition = f.tell()
+  f.seek(0, os.SEEK_END)
+  fileSize = f.tell()
+  f.seek(filePosition)
+  
+  # Calculate the bytes difference if the data section was to 
+  # start here
+  bytesMissing = (fileSize - filePosition) - totalPoints * struct.calcsize("B")
+  
+  if bytesMissing == 0:
+    pass
+  elif bytesMissing == struct.calcsize("f"):
+    fileHdr_append_v2 = _parseFile(f, wfm_header_append_v2, strict=strict)
+    fileHdr.update(fileHdr_append_v2)
+  else:
+    raise FormatError("File length is not as expected: %i bytes remaining." % (bytesMissing,))
+  
+  #import pprint
+  #pprint.pprint(fileHdr)
   
   # Read in the sample data from the scope
   dataIdx = 0
@@ -198,7 +236,9 @@ def parseRigolWFM(f, strict=True):
   
   # Other general information
   scopeData["activeChannel"] = ("CH1", "CH2", "REF", "MATH")[fileHdr["activeCh"] - 1]
-  scopeData["samplerate"] = fileHdr["smpRate"]
+  
+  if "smpRate" in fileHdr:
+    scopeData["samplerate"] = fileHdr["smpRate"]
   
   # If we are not using alternate trigger, all channels share the same trigger
   # information.

commit c4fe6f0909e13a34c3673a9e0b2871e1d5dad826

Fix parsing of WFM files with two saved channels
Commit df8ee74824 introduced a bug, since the channel2 field in
fileHdr does not always hold the length of the second channel.

wfm.py
@@ -197,9 +197,14 @@ def parseRigolWFM(f, strict=True):
   
   # Add some simple access helpers for the repeating fields
   fileHdr["channels"] = (fileHdr["channel1"], fileHdr["channel2"])
-  fileHdr["points"] = (fileHdr["points1"], fileHdr["points2"])
   fileHdr["triggers"] = (fileHdr["trigHdr1"], fileHdr["trigHdr2"])
   fileHdr["times"] = (fileHdr["time1"], fileHdr["time2"])
+  
+  # Sometimes, the channel length of the second channel is not written
+  # so the first channel has to be used.
+  fileHdr["points"] = [fileHdr["points1"], fileHdr["points2"]]
+  if fileHdr["channels"][1]["written"] and fileHdr["points"][1] == 0:
+    fileHdr["points"][1] = fileHdr["points"][0]
   
   totalPoints = 0
   for channel in range(2):
@@ -236,12 +241,7 @@ def parseRigolWFM(f, strict=True):
     if fileHdr["channels"][channel]['written']:
       #print("Channel %i written, reading it" % channel)
       nBytes = fileHdr["points"][dataIdx] * struct.calcsize("B")
-      
-      if nBytes == 0 and dataIdx==1:
-        nBytes = fileHdr['points1']
-      
-      assert nBytes > 0
-      
+    
       sampleData = array.array('B')
       sampleData.fromfile(f, nBytes)
       fileHdr["channels"][channel]['data'] = sampleData